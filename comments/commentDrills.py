efab938dbbbf1a4877b2220364225956379fd248
mariagironella <mariagironella2@gmail.com>
2021-11-21 22:03:19 -0600

    11/21/21

---
+++ comments/commentDrills.py
@@ -0,0 +1,50 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+#I like the Percy Jackson series
+
+
+#2) Make a multi-line comment.
+'''
+The 
+Heros
+of 
+Olymups
+series
+is 
+really
+good 
+too
+'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+var = 3.14 #The variable equals 3.14 aka pi
+
+
+#4) Make a single line comment.
+#3.14 is used to find the area of circles and other things
+
+
+#5) Make a multi-line comment.
+'''
+Multi-line
+comment takes up
+more than one line
+'''
+
