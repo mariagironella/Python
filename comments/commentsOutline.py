66d24dc04dd87d19d02667e2ad0a457779a08a39
mariagironella <mariagironella2@gmail.com>
2021-11-21 21:55:47 -0600

    11/21/21

---
+++ comments/commentsOutline.py
@@ -0,0 +1,23 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+#This is my single line comment practice
+
+
+#2) Make a multi-line comment.
+''' This 
+is
+my
+multi-line
+comment 
+practice
+'''
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+a = 2 #This is an integer variable
+
